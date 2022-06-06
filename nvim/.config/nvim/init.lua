77465e5c32b5fcb05ddb3c5668e3cbc46a316a71
fhchl <fhchl@users.noreply.github.com>
2022-06-06 22:49:40 +0200

    nvim: remove indentlines

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -22,7 +22,7 @@ require('packer').startup(function(use)
   use 'Shatur/neovim-ayu' -- Theme
   use 'nvim-lualine/lualine.nvim' -- Fancier statusline
   -- Add indentation guides even on blank lines
-  use 'lukas-reineke/indent-blankline.nvim'
+  -- use 'lukas-reineke/indent-blankline.nvim'
   -- Add git related info in the signs columns and popups
   use { 'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' } }
   -- Highlight, edit, and navigate code using a fast incremental parsing library
@@ -108,10 +108,10 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 })
 
 -- Indent blankline
-require('indent_blankline').setup {
-  char = '┊',
-  show_trailing_blankline_indent = false,
-}
+-- require('indent_blankline').setup {
+--   -- char = '┊',
+--   show_trailing_blankline_indent = false,
+-- }
 
 -- Gitsigns
 require('gitsigns').setup {
@@ -342,4 +342,5 @@ require('which-key').setup{}
 -- NvimTree
 require('nvim-tree').setup{}
 vim.cmd[[nnoremap <leader>e :NvimTreeToggle<CR>]]
+
 -- vim: ts=2 sts=2 sw=2 et
