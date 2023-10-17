1288d93ad13a3e3e690b0cf1ab98499d7b03b2f8
fhchl <fhchl@users.noreply.github.com>
2023-10-17 10:09:41 +0200

    nvim: don't know what that was, just saving

---
+++ nvim/.config/nvim/lua/custom/chadrc.lua
@@ -0,0 +1,3 @@
+local M = {}
+ M.ui = {theme = 'ayu_light'}
+ return M
---
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -0,0 +1,7 @@
+{
+  "neovim/nvim-lspconfig",
+   config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.plugins.lspconfig"
+   end,
+},
