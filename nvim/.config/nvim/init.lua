d65e6d6b418c036dbb87a6c7aa36fc0facff466a
fhchl <fhchl@users.noreply.github.com>
2024-05-20 18:39:01 +0200

    neovim: config python formatters

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -632,7 +632,7 @@ require('lazy').setup({
     lazy = false,
     keys = {
       {
-        '<leader>f',
+        '<leader>cf',
         function()
           require('conform').format { async = true, lsp_fallback = true }
         end,
@@ -655,7 +655,7 @@ require('lazy').setup({
       formatters_by_ft = {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
+        python = { 'isort', 'black' },
         --
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
